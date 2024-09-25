bcbfe2bcf75a0d417b13ebbc6d1b3f62a313c30f
priyankabandgar521 <priyankabandgar25@gmail.com>
2024-09-25 16:53:18 +0530

    added new task for naaptol home page

---
+++ src/main/java/pom/NaaptolHomepage.java
@@ -0,0 +1,9 @@
+package pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NaaptolHomepage {
+@FindBy(xpath="")private WebElement signin;
+@FindBy(xpath="")private WebElement search;
+}
